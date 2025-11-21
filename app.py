74f4661dfd535978df3a83d568a89006fd7e06b9
Vinnymac69 <98437312+Vinnymac69@users.noreply.github.com>
2025-11-20 16:20:43 -0800

    Create app.py

---
+++ app.py
@@ -0,0 +1,9 @@
+import streamlit as st
+import pandas as pd
+from pathlib import Path
+
+st.title("Macaroni Training Log 🍝")
+
+st.subheader("Test deployment")
+
+st.write("If you see this message, your Streamlit app is working!")
